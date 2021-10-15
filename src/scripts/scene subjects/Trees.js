cdfdfcaf2bcbe01131d532491924035428066790
cgradeff <64032507+cgradeff@users.noreply.github.com>
2021-10-15 14:41:40 -0400

    added forest, changed forest file pathh

--- src/scripts/scene subjects/Trees.js
+++ src/scripts/scene subjects/Trees.js
@@ -9,13 +9,13 @@ export class Trees {
         // this.trees = this.create();
         this.loadTrees();
         this.trees;
-        this.posObj = {
-            1: [20, 0, 10, 0],
-            2: [-25, 0, -1, 2.5],
-            3: [-18, 0, 15, 1.7],
-            4: [2, 0, 33, 1],
-            5: [37, 0, 33, 0]
-        }
+        // this.posObj = {
+        //     1: [20, 0, 10, 0],
+        //     2: [-25, 0, -1, 2.5],
+        //     3: [-18, 0, 15, 1.7],
+        //     4: [2, 0, 33, 1],
+        //     5: [37, 0, 33, 0]
+        // }
     }
 
     // create() {
@@ -32,7 +32,7 @@ export class Trees {
     loadTrees() {
         let loader = new OBJLoader();
 
-        loader.load( '../../../models/trees.obj', (object) => {
+        loader.load( '../../../models/forest.obj', (object) => {
             object.traverse( (child) => {
                 if (child instanceof THREE.Mesh) {
                     child.material = new THREE.MeshPhongMaterial( { color: "#075f61"})
@@ -42,11 +42,12 @@ export class Trees {
             this.scene.add(object);
             this.trees = object;
             object.scale.multiplyScalar(0.17);
-            object.position.z = this.posObj[this.pos][2];
-            object.position.y = this.posObj[this.pos][1];
-            object.position.x = this.posObj[this.pos][0];
-            const yDiff = this.posObj[this.pos][3]
-            object.rotation.set( 0, yDiff, 0 );
+            object.position.set(-19, 0, 50);
+            // object.position.z = this.posObj[this.pos][2];
+            // object.position.y = this.posObj[this.pos][1];
+            // object.position.x = this.posObj[this.pos][0];
+            // const yDiff = this.posObj[this.pos][3]
+            // object.rotation.set( 0, yDiff, 0 );
             // object.quaternion.x = this.posObj[this.pos][3]
             // object.castShadow = true;
             // object.receiveShadow = false;
